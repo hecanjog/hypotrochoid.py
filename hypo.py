d2bb76701212cfe5844f0001356047a90e5d9b6a
Erik Schoster <erik@hecanjog.com>
2014-06-27 18:19:41 -0500

    fixes to animation

--- hypo.py
+++ hypo.py
@@ -3,28 +3,28 @@ import random
 import math
 from pippi import dsp
 
-npoints = 1000
-ncycles = 100
-nframes = 15 * 10
-w,h = 1280, 800
+npoints = 100
+ncycles = 500
+nframes = 600
+w,h = 1280, 720 
 
-aa = dsp.breakpoint([ random.randint(1, 1000) for p in range(10) ], ncycles)
-bb = dsp.breakpoint([ random.randint(1, 40) for p in range(10) ], ncycles)
-cc = dsp.breakpoint([ random.randint(1, 40) for p in range(10) ], ncycles)
+aa = dsp.breakpoint([ random.randint(1, 100) for p in range(10) ], ncycles)
+bb = dsp.breakpoint([ random.randint(1, 20) for p in range(10) ], ncycles)
+cc = dsp.breakpoint([ random.randint(1, 20) for p in range(10) ], ncycles)
 
 Rs = dsp.breakpoint([ random.randint(200, 255) for p in range(4) ], ncycles)
 Gs = dsp.breakpoint([ random.randint(0, 255) for p in range(3) ], ncycles)
 Bs = dsp.breakpoint([ random.randint(200, 255) for p in range(5) ], ncycles)
 
-background = (random.randint(100, 200), random.randint(0, 255), random.randint(100, 200))
+background = (random.randint(50, 200), random.randint(0, 255), random.randint(50, 200))
 
-choosepoints = [ random.randint(0, 500) for p in range(ncycles * npoints * nframes) ]
+choosepoints = [ random.randint(0, 500) for p in range(ncycles * npoints) ]
 
 y1ss = [ dsp.breakpoint([ random.randint(1, 50) for p in range(10) ], npoints) for s in range(ncycles) ]
 x1ss = [ dsp.breakpoint([ random.randint(1, 50) for p in range(10) ], npoints) for s in range(ncycles) ]
 
 counter = 0
-for frame in range():
+for frame in range(nframes):
     i = Image.new('RGB', (w, h), background)
 
     draw = ImageDraw.Draw(i)
@@ -34,9 +34,9 @@ for frame in range():
         G = int(Gs[t])
         B = int(Bs[t])
 
-        a = aa[t]
-        b = bb[t]
-        c = cc[t]
+        a = (aa[t] + frame) % w + 1
+        b = (bb[t] + frame) % 60 + 1
+        c = (cc[t] + frame) % 30 + 1
 
         yoffset, xoffset = h / 2, w / 4 
 
@@ -59,4 +59,6 @@ for frame in range():
 
             counter += 1
 
-    i.save('frames/hypo%i004.jpg' % frame)
+    i.save('frames/hypo%04d.png' % frame)
+
+
